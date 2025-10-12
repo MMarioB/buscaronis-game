f1208791a535ef6fc36858db08d55f915056954b
Mario Bravo Ovejero <mmariob196@gmail.com>
2025-10-12 17:26:29 +0200

    fix: Externalize sharp module

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,24 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+
+  experimental: {
+    serverComponentsExternalPackages: ['sharp'],
+  },
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      try {
+        config.externals.push({
+          sharp: 'commonjs sharp',
+        });
+      } catch (e) {
+        config.externals = { sharp: 'commonjs sharp' };
+        console.log(e);
+      }
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
