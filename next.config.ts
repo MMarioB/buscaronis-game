839cebf0a19b9a5588670a81bb7ebad7e8ace1bb
Mario Bravo Ovejero <mmariob196@gmail.com>
2025-10-12 17:08:40 +0200

    fix: deploy vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/buscaronis-game',
-  assetPrefix: '/buscaronis-game/',
   images: {
     unoptimized: true,
   },
